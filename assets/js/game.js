770ef67c8541430ca081f6390fd1cfd3e0c17b5e
Samantha Capps <samantha@Cheez.local>
2017-03-01 20:09:53 -0500

    working on paralyze

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,13 @@ music.loop = true;
 
 var enemyname = document.getElementById("enemyname");
 var enemylvl = document.getElementById("enemylvl");
+var enemystatus = document.getElementById("enemystatus");
 var enemyhpbarfull = document.getElementById("enemyhpbarfull");
 var enemyhp = document.getElementById("enemyhp");
 var enemyimg = document.getElementById("enemyimg");
 var heroname = document.getElementById("heroname");
 var herolvl = document.getElementById("herolvl");
+var herostatus = document.getElementById("herostatus");
 var herohpbarfull = document.getElementById("herohpbarfull");
 var herohp = document.getElementById("herohp");
 var heroimg = document.getElementById("heroimg");
@@ -16,12 +18,14 @@ var options = document.getElementById("options");
 var move1 = "";
 var fight = document.getElementById("fight");
 var win = document.getElementById("win");
-var random = 0;
+var rando = 0;
+var placeholder = "";
 
 function tacklehero() {
 		enemy.hpnow = enemy.hpnow - 5 + enemy.defense
 		enemyhp.textContent = "HP " + enemy.hpnow + "/" + enemy.hptotal;
 		enemyhpbarfull.style.width = enemy.hpnow / enemy.hptotal * 100 + "%";
+		setTimeout(options, 2000);
 
 }
 
@@ -29,31 +33,51 @@ function tackleenemy() {
 		hero.hpnow = hero.hpnow - 5 + hero.defense
 		herohp.textContent = "HP " + hero.hpnow + "/" + hero.hptotal;
 		herohpbarfull.style.width = hero.hpnow / hero.hptotal * 100 + "%";
+		setTimeout(options, 2000);
 }
 
 
 function tailwhiphero() {
 	enemy.defense--;
+	setTimeout(options, 2000);
 }
 
 function tailwhipenemy() {
 	enemy.defense--;
+	setTimeout(options, 2000);
 }
 
 function electricshockenemy() {
 	hero.hpnow = hero.hpnow - 10 + hero.defense;
 	herohp.textContent = "HP " + hero.hpnow + "/" + hero.hptotal;
 	herohpbarfull.style.width = hero.hpnow / hero.hptotal * 100 + "%";
+	rando = Math.random();
+	if (rando < .25) {
+		placeholder = function() {		
+			hero.status = "paralyzed";
+			herostatus.textContent = "paralyzed";
+			opt0.textContent = hero.name + " is paralyzed";
+			opt1.textContent = "and can't move!";
+			opt2.textContent = "";
+			opt3.textContent = "";
+		}
+		setTimeout(placeholder, 2000);
+		setTimeout(options, 4000);
+	}
+	else {
+		setTimeout(options, 2000);
+	}
 }
 
 function psychichero() {
 	enemy.hpnow = enemy.hpnow - 10 + hero.defense;
 	enemyhp.textContent = "HP " + enemy.hpnow + "/" + enemy.hptotal;
 	enemyhpbarfull.style.width = enemy.hpnow / enemy.hptotal * 100 + "%";
+	setTimeout(options, 2000);
 }
 
-var enemy = {name: "Pikachu", lvl: 10, hpnow: 60, hptotal: 60, defense: 3, img: "assets/images/pikachufront.png", moves: [{name: "Tackle", effect: function() {tackleenemy()}}, {name: "Tail Whip", effect: function() {tailwhipenemy}}, {name: "Electric Shock", effect: function() {electricshockenemy()}}, ""]}
-var hero = {name: "Mew", lvl: 15, hpnow: 75, hptotal: 75, defense: 3, img: "assets/images/mewback.png", moves: [{name: "Tackle", effect: function() {tacklehero()}}, {name: "Tail Whip", effect: function() {tailwhiphero()}}, {name: "Psychic", effect: function() {psychichero()}}, ""]}
+var enemy = {name: "Pikachu", type: "electric", status: "", lvl: 10, hpnow: 60, hptotal: 60, defense: 3, attack: 2, img: "assets/images/pikachufront.png", moves: [{name: "Tackle", effect: function() {tackleenemy()}}, {name: "Tail Whip", effect: function() {tailwhipenemy}}, {name: "Electric Shock", effect: function() {electricshockenemy()}}, ""]}
+var hero = {name: "Mew", type: "psychic", status: "", lvl: 15, hpnow: 75, hptotal: 75, defense: 3, attack: 2, img: "assets/images/mewback.png", moves: [{name: "Tackle", effect: function() {tacklehero()}}, {name: "Tail Whip", effect: function() {tailwhiphero()}}, {name: "Psychic", effect: function() {psychichero()}}, ""]}
 
 function randomizer() {
 	random = Math.floor(Math.random() * 4);
@@ -62,8 +86,24 @@ function randomizer() {
 	}
 }
 
+function lose() {
+	opt0.textContent = "YOU LOSE!";
+	opt1.textContent = "";
+	opt2.textContent = "";
+	opt3.textContent = "";
+	music.pause();
+	opt0.onclick = function() {
+	}
+	opt1.onclick = function() {
+	}		
+	opt2.onclick = function() {
+	}		
+	opt3.onclick = function() {
+	}
+}
+
 function enemymove() {
-	if (enemy.hpnow == 0) {
+	if (enemy.hpnow <= 0) {
 		opt0.textContent = "YOU WIN!";
 		opt1.textContent = "";
 		opt2.textContent = "";
@@ -81,23 +121,45 @@ function enemymove() {
 	}
 	else {
 		randomizer();
-		enemy.moves[random].effect();
+		enemy.moves[2].effect();
 		opt0.textContent = enemy.name + " used " + enemy.moves[random].name + "!";
 		opt1.textContent = "";
 		opt2.textContent = "";
 		opt3.textContent = "";
-		setTimeout(options, 2000);
+		if (hero.hpnow <= 0) {
+			setTimeout(lose, 2000);
+		}
 	}
 }
 
 function domove(x) {
-	fight.play();
-	hero.moves[x].effect();
-	opt0.textContent = hero.name + " used " + hero.moves[x].name + "!";
-	opt1.textContent = "";
-	opt2.textContent = "";
-	opt3.textContent = "";
-	setTimeout (enemymove, 2000);
+	if (hero.status == "paralyzed") {
+		random = Math.random();
+		if (random < .45) {
+			opt0.textContent = hero.name + " is no longer";
+			opt1.textContent = "paralyzed!";
+			opt2.textContent = "";
+			opt3.textContent = "";
+			hero.status = "";
+			herostatus.textContent = "";
+			setTimeout(domove, 2000);
+		}
+		else {
+			opt0.textContent = hero.name + " is paralyzed";
+			opt1.textContent = "and can't move!";
+			opt2.textContent = "";
+			opt3.textContent = "";
+		}
+	}
+	else {
+		fight.play();
+		hero.moves[x].effect();
+		opt0.textContent = hero.name + " used " + hero.moves[x].name + "!";
+		opt1.textContent = "";
+		opt2.textContent = "";
+		opt3.textContent = "";
+		setTimeout (enemymove, 2000);
+	}
 }
 
 enemyname.textContent = enemy.name;
